1b3211ca9284bbc38ba973efb9462fec5fcaebc8
sephania96 <162960499+sephania96@users.noreply.github.com>
2025-07-13 23:42:47 +0100

    Delete crm directory

--- crm/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# crm/admin.py
-from .models import Customer, Product, Order
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
